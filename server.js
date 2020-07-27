16d7a848de70990d63477e3cda2e5d5928dbb7dc
Johnny Li <reptile18@gmail.com>
2020-07-27 11:13:48 -0700

    initial deploy to heroku

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workout", {
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout"
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
